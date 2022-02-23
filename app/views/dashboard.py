70da38f719ad64b3b09353be3b28e26a0cdc6c37
chick_0 <chick_0@ch1ck.xyz>
2022-02-23 23:21:40 +0900

    add more information in dashboard

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -15,10 +15,18 @@ def return_info(user: User):
         owner_id=user.id
     ).count()
 
+    enc_memo_count = Memo.query.filter_by(
+        owner_id=user.id,
+        encrypted=True
+    ).count()
+
     return resp_json(
         data={
             "creation_date": round(user.creation_date.timestamp()),
             "tos_agree_date": round(user.tos_agree.timestamp()),
             "memo_count": memo_count,
+            "enc_memo_count": enc_memo_count,
+            "last_login": round(user.last_login.timestamp()),
+            "admin": user.is_admin,
         }
     )
